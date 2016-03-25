e69c7f8b50c1aac92b2c60f0cd911e707ee9d370
CurlyYang <eric.y@inwinstack.com>
2016-03-25 18:02:05 +0800

    add constants

---
+++ lib/constants.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OCA\Files_Report;
+
+
+class Constants {
+    const SPAM_FILE = 1;
+    const PORM_PHOTO_OR_FILE = 2;
+    const CONTAINS_BAD_WORDS = 3;
+
+    const PENDING_STATE = 0;
+    const REPORT_STATE = 1;
+    const CANCEL_STATE = 2;
+}
+
+
+
+
+
+?>
