80293a5d29b1ac3d35c5582a61e2246ba57f7b3c
CurlyYang <eric.y@inwinstack.com>
2016-03-25 18:02:35 +0800

    add force delete class

---
+++ lib/forcedelete.php
@@ -0,0 +1,133 @@
+<?php
+namespace OCA\Files_Report;
+
+class ForceDelete {
+    /**
+    * Force owner's delete file/folder by admin role.
+    *
+    * @param string $owner
+    * @param string $file
+    * @return bool
+    */
+    public static function forceDeleteOwnerFile($owner,$file){
+       \OCP\Util::writeLog('Duncan','=$owner='.$owner, \OCP\Util::ERROR);
+       \OCP\Util::writeLog('Duncan','=$file='.$file, \OCP\Util::ERROR);
+       //$file = files/1.txt
+       //$file = files/2
+       //$file = files/3/1.txt
+
+       //$owner = 'test';
+       //$file = 'files/1.txt';
+
+       //Prepare filter the file path.
+       $dirs = explode("/", $file);
+       $rootDir = $dirs[0];
+       $filterFilePath = preg_replace("/^$rootDir\//",'',$file);
+
+       // Step1: Init Mountpoints and new owner's view.
+       //\OC\Files\Filesystem::tearDown();
+       \OC\Files\Filesystem::initMountPoints($owner);
+       $view = new \OC\Files\View("/$owner/files");
+       
+       // Step2: Unclock the file's lock.
+       $mount = $view->getMount($filterFilePath);
+       $storage = $mount->getStorage();
+       $id = $storage->getId();
+       $key = 'files/' . md5($id . '::' . trim($file, '/'));
+       $sql = 'UPDATE `*PREFIX*file_locks` SET `lock` = 0 WHERE `key` = ?';
+
+       $query = \OCP\DB::prepare($sql);
+       //$result = $query->execute(array($key));
+       if (!$query->execute(array($key))){
+           \OCP\Util::writeLog('module name',"When force clean file ($filterFilePath) lock status failed.", \OCP\Util::INFO);
+           return false;
+       }
+
+       if (!$view->unlockFile($filterFilePath,\OCP\Lock\ILockingProvider::LOCK_EXCLUSIVE)){
+           \OCP\Util::writeLog('module name',"When force unlock file ($filterFilePath) failed.", \OCP\Util::INFO);
+           return false;
+       }
+
+       // Step3: Delete the file's priview file.
+       self::deletePreviewFiles($owner,$view,$file);
+
+
+       // Step4: Delete the file's veriosn file.
+       if(!\OC_App::isEnabled('files_versions')){
+           self::deleteVersionFiles($owner,$filterFilePath);
+       }
+
+       // Step5: Delete the file.
+       if (!$view->unlink($filterFilePath) || $view->file_exists($filterFilePath)){
+           \OCP\Util::writeLog('module name',"When force delete $filterFilePath file failed.", \OCP\Util::INFO);
+           return false;
+       }
+
+       return true;
+    }
+    /**
+    * Force owner's delete version files by admin role.
+    *
+    * @param string $owner
+    * @param string $file
+    * @return bool
+    */
+    public static function deleteVersionFiles($owner,$file){
+       //$file = 1.txt
+       //$file = 2
+       //$file = 3/1.txt
+       $view = new \OC\Files\View("/$owner");
+
+       if($view->is_dir('files_versions' . '/' . $file)){
+           $files = $view->getDirectoryContent('files_versions' . '/' . $file);
+
+
+           foreach($files as $fileArray) {
+               $filename = $fileArray['name'];
+               $filePath = $file . '/' . $filename;
+                   self::deleteVersionFiles($owner,$view,$filePath);
+           }
+           $view->unlink('files_versions' . '/' . $file);
+       }
+       else{
+
+           $versions = OCA\Files_Versions\Storage::getVersions($owner,$file);
+           if (!empty($versions)) {
+               foreach ($versions as $v) {
+                   self::deletePreviewFiles($owner,$view,'files_versions/'. $file . ".v".$v['version']);
+                   $view->unlink('files_versions/'. $file . ".v".$v['version']);
+               }
+           }
+       }
+    }
+    /**
+    * Force owner's delete preview files by admin role.
+    *
+    * @param string $owner
+    * @param string $file
+    * @return bool
+    */
+    public static function deletePreviewFiles($owner,$view,$file){
+       //$file = files/1.txt
+       //$file = files/2
+       //$file = files/3/1.txt
+
+       $dirs = explode("/", $file);
+       $rootDir = $dirs[0];
+       $filterFilePath = preg_replace("/^$rootDir\//",'',$file);
+
+       if($view->is_dir($filterFilePath)){
+           $files = $view->getDirectoryContent($filterFilePath);
+           foreach($files as $fileArray) {
+               $filename = $fileArray['name'];
+               $filePath = $file . '/' . $filename;
+               self::deletePreviewFiles($owner,$view,$filePath);
+           }
+       }
+       else{
+           $preview = new \OC\Preview($owner, $rootDir,preg_replace("/^$rootDir\//",'',$file));
+           $preview->deleteAllPreviews();
+       }
+
+    }
+}
