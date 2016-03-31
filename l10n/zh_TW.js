da1b3abca0d0ba533a85386f622bb0672de0df63
CurlyYang <eric.y@inwinstack.com>
2016-03-31 11:51:24 +0800

    translation file

---
+++ l10n/zh_TW.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "files_report",
+    {
+    "Your action is success"  : "操作成功",
+    "Your report will be send to administractor." : "您的檢舉將會回報給管理者",
+    "Reports" : "檢舉",
+    "Include bad words or graphs." : "包含不雅文字或圖片",
+    "This is a uncomfortable file." : "這個檔案令人不舒服",
+    "I think it shouldn't be on custum cloud." : "我認為這不應該出現在儲存應用服務上",
+    "It's spam file." : "這是垃圾檔案",
+    "Submit" : "送出"
+    },
+"nplurals=1; plural=0;");
